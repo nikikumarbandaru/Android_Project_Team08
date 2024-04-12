2eced2b8625cf2bd7261bd9b338be4f1b21ad64b
Sahithi Yamasani <71246202+SahithiYamasani@users.noreply.github.com>
2024-04-11 19:39:29 -0500

    Updated new code MainActivity.java

--- Team08_MusicPlayerApplication/app/src/main/java/com/example/team08_musicplayerapplication/MainActivity.java
+++ Team08_MusicPlayerApplication/app/src/main/java/com/example/team08_musicplayerapplication/MainActivity.java
@@ -1,35 +1,38 @@
 package com.example.team08_musicplayerapplication;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private ActivityResultLauncher<Intent> mStartForResult;
+    private RecyclerView recyclerView;
+    private MusicAdapter musicAdapter;
+    private List<MusicModel.Music> musicList;
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-    }
-    public void sendOnClick(View view)
-    {
-        Intent intent = new Intent(this, SongsList.class);
-
-
-        startActivity(intent);
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        musicList = new ArrayList<>();
+        musicList.add(new MusicModel.Music("Too Sad to Dance", "Jung Kook",R.drawable.toosadtodance,R.raw.toosadtodance));
+        musicList.add(new MusicModel.Music("Life goes on", "BTS",R.drawable.lifegoeson,R.raw.lifegoeson));
+        musicList.add(new MusicModel.Music("The Astronaut", "Jin",R.drawable.astronaut,R.raw.astronaut));
+        musicList.add(new MusicModel.Music("Equal Sign", "J-Hope",R.drawable.equalsign,R.raw.equalsign));
+        musicList.add(new MusicModel.Music("Amygdala", "AgustD",R.drawable.amygdala,R.raw.amygdala));
+        musicList.add(new MusicModel.Music("Promise", "Jimin",R.drawable.promise,R.raw.promise));
+        musicList.add(new MusicModel.Music("Sweet Night", "V",R.drawable.sweetnight,R.raw.sweetnight));
+        musicList.add(new MusicModel.Music("Akuma No ko", "Ai Higuchi",R.drawable.akumanoko,R.raw.akumanoko));
+        musicList.add(new MusicModel.Music("Fast Car", "Luke Combs",R.drawable.fastcar,R.raw.fastcar));
+        musicList.add(new MusicModel.Music("All too well", "Taylor Swift",R.drawable.alltoowell,R.raw.alltoowell));
+        musicList.add(new MusicModel.Music("Wild Flower", "RM",R.drawable.wildflower,R.raw.wildflower));
+        musicList.add(new MusicModel.Music("Shallow", "Lady Gaga & Bradley Cooper",R.drawable.shallow,R.raw.shallow));
+        musicAdapter = new MusicAdapter(musicList, MainActivity.this);
+        recyclerView.setAdapter(musicAdapter);
     }
 }
