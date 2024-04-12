1a68569fc3ba60f21c00aa6ec7fa42d7354f2817
Nikith Kumar Bandaru <142968177+nikikumarbandaru@users.noreply.github.com>
2024-04-11 21:28:09 -0500

    Nikith Kumar Bandaru || Added import statemets

--- Team08_MusicPlayerApplication/app/src/main/java/com/example/team08_musicplayerapplication/MainActivity.java
+++ Team08_MusicPlayerApplication/app/src/main/java/com/example/team08_musicplayerapplication/MainActivity.java
@@ -8,6 +8,10 @@ import android.os.Bundle;
 
 import java.util.ArrayList;
 import java.util.List;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
