b2054f96096aa8611d11b0fb40061dba9a3f2fa8
Sahithi Yamasani <71246202+SahithiYamasani@users.noreply.github.com>
2024-04-11 19:41:29 -0500

    updated code

---
+++ Team08_MusicPlayerApplication/app/src/main/java/com/example/MusicModel.java
@@ -0,0 +1,31 @@
+package com.example.team08_musicplayerapplication;
+
+import java.io.Serializable;
+
+public class MusicModel  {
+    public static class Music implements Serializable{
+        private String title;
+        private String artist;
+        private int image;
+        private int song;
+        public Music(String title, String artist,int image,int song) {
+            this.title = title;
+            this.artist = artist;
+            this.image=image;
+            this.song=song;
+        }
+        public String getTitle() {
+            return title;
+        }
+        public String getArtist() {
+            return artist;
+        }
+        public int getImage() {
+            return image;
+        }
+        public int getSong() {
+            return song;
+        }
+    }
+}
+
